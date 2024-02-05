76eb7af76bcea8ac6dda016d87e0a666dd7b71a4
Edo <hello@edoardo-hb.it>
2024-02-05 20:19:08 +0100

    feat: created classes

---
+++ models.php
@@ -0,0 +1,50 @@
+<?php
+class Prodotto
+{
+  public $id;
+  public $nome;
+  public $prezzo;
+  public $categoria;
+
+  public function __construct($id, $nome, $prezzo, $categoria)
+  {
+    $this->id = $id;
+    $this->nome = $nome;
+    $this->prezzo = $prezzo;
+    $this->categoria = $categoria;
+  }
+}
+
+class ProdottoAlimentare extends Prodotto
+{
+  public $tipo;
+
+  public function __construct($id, $nome, $prezzo, $categoria, $tipo)
+  {
+    parent::__construct($id, $nome, $prezzo, $categoria);
+    $this->tipo = $tipo;
+  }
+}
+
+class ProdottoAccessorio extends Prodotto
+{
+  public $materiale;
+
+  public function __construct($id, $nome, $prezzo, $categoria, $materiale)
+  {
+    parent::__construct($id, $nome, $prezzo, $categoria);
+    $this->materiale = $materiale;
+  }
+}
+
+class Categoria
+{
+  public $id;
+  public $nome;
+
+  public function __construct($id, $nome)
+  {
+    $this->id = $id;
+    $this->nome = $nome;
+  }
+}
